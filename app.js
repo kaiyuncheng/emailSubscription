1b7638a51708cf547ca1a251d7ec145994964b94
KaiYun Cheng <tanyacheng@gmail.com>
2020-12-02 17:40:04 +0800

    changed to EJS

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
-const { request } = require('express');
-// const request = require('request');
+const validator = require(__dirname + '/validator.js');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let msg = '';
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/signup.html');
-}); 
+  res.render('signup',{
+    msg : msg,
+  });
+});
 
+function checkRequired(inputAry) {
+    inputAry.forEach(function (input) {
+      if (input === '') {
+        msg = 'All fields are required or Email is not valid.';
+      } else {
+        msg = '';
+      }
+    });
+  }
+
+  function checkEmail(input) {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (!re.test(String(input).toLowerCase())) {
+    msg = 'All fields are required or Email is not valid.';
+  } else {
+    msg = '';
+  }
+  };
 app.post('/', function (req, res) {
-
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
-
   const data = {
     members: [
       {
@@ -41,18 +61,24 @@ app.post('/', function (req, res) {
 
   const requestHttps = https.request(url, options, function (response) {
     if (response.statusCode === 200){
-      res.sendFile(__dirname + '/success.html');
+      res.render('success');
     }else{
-      res.sendFile(__dirname + '/failure.html');
+      res.render('failure');
     }
     response.on('data', function(data){
      console.log(JSON.parse(data));
     })
   })
 
-  requestHttps.write(jsonData);
-  requestHttps.end();
+  checkRequired([firstName, lastName, email]);
+  checkEmail(email);
 
+  if (msg !== '') {
+    res.redirect('/');
+  }else{
+    requestHttps.write(jsonData);
+  }
+  requestHttps.end();
 });
 
 app.post('/failure', function (req, res) {
